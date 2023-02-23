5b9c0ae41006b90ad59ef85dccf08e985161fd69
Ike Ximenez <isaac@infomakina.net>
2023-02-23 21:26:19 +0000

    prueba x2

--- models/Fact_sales_details_p_21.sql
+++ models/Fact_sales_details_p_21.sql
@@ -1,4 +1,4 @@
 select fact.*, dim_item.rcat as ORIG_RCAT, dim_item.vendor_id as ORIG_VENDOR, dim_item.abccode as ORIG_ABCCODE
 from resideo.core_dwh.pre_fact_sales_detail fact
 left join resideo.core_dwh.pre_dim_item_2 dim_item
- on fact.item_id = dim_item.item_id
+ on fact.item_id = dim_item.item_id 
